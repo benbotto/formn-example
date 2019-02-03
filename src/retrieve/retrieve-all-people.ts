a24accd2f1a488bb3e2252f3429bb92100cf4fb9
Ben Botto <benjamin.botto@gmail.com>
2019-02-03 14:59:20 -0800

    Adds some comments/cleanup to the retrieve example.

--- src/retrieve/retrieve-all-people.ts
+++ src/retrieve/retrieve-all-people.ts
@@ -1,25 +1,30 @@
+import { inspect } from 'util';
+
 import { MySQLDataContext, ConnectionOptions, Select } from 'formn';
 
 import { Person } from '../entity/person.entity';
 
-const connOpts: ConnectionOptions = require('../../connections.json');
-
 async function main() {
+  const connOpts: ConnectionOptions = require('../../connections.json');
   const dataContext = new MySQLDataContext();
 
   try {
     await dataContext.connect(connOpts);
 
+    // Create a Select query, which will pull all People records from the
+    // people table.
     const query: Select<Person> = dataContext
       .from(Person)
       .select();
 
+    // Log the generated SQL.
     console.log(query.toString());
 
+    // Execute the query to get all Person records.
     const people: Person[] = await query
       .execute();
 
-    console.log(JSON.stringify(people, null, 2));
+    console.log(inspect(people, {depth: null, compact: false}));
 
     await dataContext.end();
   }

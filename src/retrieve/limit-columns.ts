61249395281dac859eb5eae2bc251bbc85638735
Ben Botto <benjamin.botto@gmail.com>
2019-02-04 20:00:56 -0800

    Adds a limiting columns example.

---
+++ src/retrieve/limit-columns.ts
@@ -0,0 +1,34 @@
+import { inspect } from 'util';
+
+import { MySQLDataContext, ConnectionOptions, Select } from 'formn';
+
+import { Person } from '../entity/person.entity';
+
+async function main() {
+  const connOpts: ConnectionOptions = require('../../connections.json');
+  const dataContext = new MySQLDataContext();
+
+  try {
+    await dataContext.connect(connOpts);
+
+    // Only pull Person.id and Person.firstName.
+    const query: Select<Person> = dataContext
+      .from(Person, 'p')
+      .select('p.id', 'p.firstName');
+
+    console.log(query.toString());
+
+    const people: Person[] = await query
+      .execute();
+
+    console.log(inspect(people, {depth: null, compact: false}));
+
+    await dataContext.end();
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
+
+main();
+

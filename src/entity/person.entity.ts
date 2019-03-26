2b9e06416b94eedd055e76dca22cccc61c182b81
Ben Botto <benjamin.botto@gmail.com>
2019-03-26 09:43:25 -0700

    Adds sqlDataType to model columns.

--- src/entity/person.entity.ts
+++ src/entity/person.entity.ts
@@ -4,16 +4,16 @@ import { PhoneNumber } from './phone-number.entity';
 
 @Table({name: 'people'})
 export class Person {
-  @Column({hasDefault: true, isNullable: false})
+  @Column({hasDefault: true, isNullable: false, sqlDataType: 'timestamp'})
   createdOn: Date;
 
-  @Column({maxLength: 255})
+  @Column({maxLength: 255, sqlDataType: 'varchar'})
   firstName: string;
 
-  @Column({maxLength: 255})
+  @Column({maxLength: 255, sqlDataType: 'varchar'})
   lastName: string;
 
-  @Column({name: 'personID', isPrimary: true, isGenerated: true, isNullable: false})
+  @Column({name: 'personID', isPrimary: true, isGenerated: true, isNullable: false, sqlDataType: 'int'})
   id: number;
 
   @OneToMany<Person, PhoneNumber>(() => PhoneNumber, (l, r) => [l.id, r.personId])
--- src/entity/phone-number.entity.ts
+++ src/entity/phone-number.entity.ts
@@ -4,16 +4,16 @@ import { Person } from './person.entity';
 
 @Table({name: 'phone_numbers'})
 export class PhoneNumber {
-  @Column({name: 'personID', isNullable: false})
+  @Column({name: 'personID', isNullable: false, sqlDataType: 'int'})
   personId: number;
 
-  @Column({isNullable: false, maxLength: 255})
+  @Column({isNullable: false, maxLength: 255, sqlDataType: 'varchar'})
   phoneNumber: string;
 
-  @Column({name: 'phoneNumberID', isPrimary: true, isGenerated: true, isNullable: false})
+  @Column({name: 'phoneNumberID', isPrimary: true, isGenerated: true, isNullable: false, sqlDataType: 'int'})
   id: number;
 
-  @Column({maxLength: 255})
+  @Column({maxLength: 255, sqlDataType: 'varchar'})
   type: string;
 
   @ManyToOne<PhoneNumber, Person>(() => Person, (l, r) => [l.personId, r.id])

2cdc57d7bada7c3b07bf475c894e211c2d6ebde8
Ben Botto <benjamin.botto@gmail.com>
2019-07-08 16:12:35 -0700

    Creates dummy people entries.

---
+++ migrations/2019-07-08__12-26-05-400__insert_people.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const people = [
+  {firstName: 'Joe', lastName: 'Shmo'},
+  {firstName: 'Rand', lastName: 'AlThore'},
+  {firstName: 'Holly', lastName: 'Davis'},
+  {firstName: 'Jenny', lastName: 'Mather'},
+];
+
+module.exports = {
+  /**
+   * Run the migration.
+   */
+  async up(dataContext) {
+    const results = [];
+
+    for (let i = 0; i < people.length; ++i) {
+      const sql    = 'INSERT INTO people (firstName, lastName) VALUES (:firstName, :lastName)';
+      const params = people[i];
+
+      console.log(sql);
+      console.log(params);
+
+      const result = await dataContext
+        .getExecuter()
+        .query(sql, params);
+
+      results.push(result);
+    }
+
+    return results;
+  },
+
+  /**
+   * Bring down a migration.
+   */
+  async down(dataContext) {
+    const results = [];
+
+    for (let i = 0; i < people.length; ++i) {
+      const sql    = 'DELETE FROM people WHERE firstName = :firstName AND lastName = :lastName';
+      const params = people[i];
+
+      console.log(sql);
+      console.log(params);
+
+      const result = await dataContext
+        .getExecuter()
+        .query(sql, params);
+
+      results.push(result);
+    }
+
+    return results;
+  }
+};
